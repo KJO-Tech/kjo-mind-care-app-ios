565bc864791e371aefaeacff579a8eff029d0590
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2025-11-16 14:57:59 -0500

    Initial Commit

---
+++ KJOMindCare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KJOMindCare
+//
+//  Created by Yisus on 16/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KJOMindCare/KJOMindCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  KJOMindCareApp.swift
+//  KJOMindCare
+//
+//  Created by Yisus on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct KJOMindCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
