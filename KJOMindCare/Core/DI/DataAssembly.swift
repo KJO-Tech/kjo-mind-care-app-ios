69c1ef46c92594663638f454ae742796f7c14488
Kevin-BQ <kevinalexanderbricenoquezada@gmail.com>
2025-12-04 13:45:57 -0500

    feat(di): add use cases and repositories to domain and data assemblies with Swinject

--- KJOMindCare/Core/DI/DataAssembly.swift
+++ KJOMindCare/Core/DI/DataAssembly.swift
@@ -18,9 +18,52 @@ final class DataAssembly: Assembly {
         }.inObjectScope(.container)
         
         container.register(AuthRepository.self){ r in
-            
-            AuthRepositoryImpl(authService: r.resolve(AuthFirebaseService.self)!, firestoreService: r.resolve(FireStoreService.self)!)
-            
+            AuthRepositoryImpl(
+                authService: r.resolve(AuthFirebaseService.self)!,
+                firestoreService: r.resolve(FireStoreService.self)!
+            )
+        }
+        
+        // Blog Repository
+        container.register(BlogRepository.self) { r in
+            BlogRepositoryImpl(
+                firestoreService: r.resolve(FireStoreService.self)!
+            )
+        }
+        
+        // Category Repository
+        container.register(CategoryRepository.self) { r in
+            CategoryRepositoryImpl(
+                firestoreService: r.resolve(FireStoreService.self)!
+            )
+        }
+        
+        // Daily Activity Repository
+        container.register(DailyActivityRepository.self) { r in
+            DailyActivityRepositoryImpl(
+                firestoreService: r.resolve(FireStoreService.self)!
+            )
+        }
+        
+        // Activity Subscription Repository
+        container.register(ActivitySubscriptionRepository.self) { r in
+            ActivitySubscriptionRepositoryImpl(
+                firestoreService: r.resolve(FireStoreService.self)!
+            )
+        }
+        
+        // Reaction Repository
+        container.register(ReactionRepository.self) { r in
+            ReactionRepositoryImpl(
+                firestoreService: r.resolve(FireStoreService.self)!
+            )
+        }
+        
+        // Comment Repository
+        container.register(CommentRepository.self) { r in
+            CommentRepositoryImpl(
+                firestoreService: r.resolve(FireStoreService.self)!
+            )
         }
     }
 }
--- KJOMindCare/Core/DI/DomainAssembly.swift
+++ KJOMindCare/Core/DI/DomainAssembly.swift
@@ -9,6 +9,7 @@ import Swinject
 
 final class DomainAssembly: Assembly {
     func assemble(container: Container) {
+        // Auth Use Cases
         container.register(LoginUseCase.self){ r in
             LoginUseCase(
                 repository: r.resolve(AuthRepository.self)!
@@ -18,6 +19,179 @@ final class DomainAssembly: Assembly {
         container.register(RegisterUseCase.self){ r in
             RegisterUseCase(
                 repository: r.resolve(AuthRepository.self)!
+            )
+        }
+        
+        // Blog Use Cases
+        container.register(GetBlogPostsUseCase.self) { r in
+            GetBlogPostsUseCase(
+                repository: r.resolve(BlogRepository.self)!
+            )
+        }
+        
+        container.register(GetBlogByIdUseCase.self) { r in
+            GetBlogByIdUseCase(
+                repository: r.resolve(BlogRepository.self)!
+            )
+        }
+        
+        container.register(CreateBlogUseCase.self) { r in
+            CreateBlogUseCase(
+                repository: r.resolve(BlogRepository.self)!
+            )
+        }
+        
+        container.register(UpdateBlogUseCase.self) { r in
+            UpdateBlogUseCase(
+                repository: r.resolve(BlogRepository.self)!
+            )
+        }
+        
+        container.register(UpdateBlogStatusUseCase.self) { r in
+            UpdateBlogStatusUseCase(
+                repository: r.resolve(BlogRepository.self)!
+            )
+        }
+        
+        container.register(GetUserPostsCountUseCase.self) { r in
+            GetUserPostsCountUseCase(
+                repository: r.resolve(BlogRepository.self)!
+            )
+        }
+        
+        // Category Use Cases
+        container.register(GetCategoriesUseCase.self) { r in
+            GetCategoriesUseCase(
+                repository: r.resolve(CategoryRepository.self)!
+            )
+        }
+        
+        container.register(GetCategoryByIdUseCase.self) { r in
+            GetCategoryByIdUseCase(
+                repository: r.resolve(CategoryRepository.self)!
+            )
+        }
+        
+        container.register(CreateCategoryUseCase.self) { r in
+            CreateCategoryUseCase(
+                repository: r.resolve(CategoryRepository.self)!
+            )
+        }
+        
+        container.register(DeleteCategoryUseCase.self) { r in
+            DeleteCategoryUseCase(
+                repository: r.resolve(CategoryRepository.self)!
+            )
+        }
+        
+        // Daily Activity Use Cases
+        container.register(GetActivityCategoriesUseCase.self) { r in
+            GetActivityCategoriesUseCase(
+                repository: r.resolve(DailyActivityRepository.self)!
+            )
+        }
+        
+        container.register(GetExercisesByCategoryUseCase.self) { r in
+            GetExercisesByCategoryUseCase(
+                repository: r.resolve(DailyActivityRepository.self)!
+            )
+        }
+        
+        container.register(GetExerciseByIdUseCase.self) { r in
+            GetExerciseByIdUseCase(
+                repository: r.resolve(DailyActivityRepository.self)!
+            )
+        }
+        
+        container.register(GetAllExercisesUseCase.self) { r in
+            GetAllExercisesUseCase(
+                repository: r.resolve(DailyActivityRepository.self)!
+            )
+        }
+        
+        container.register(GetUserSubscriptionsUseCase.self) { r in
+            GetUserSubscriptionsUseCase(
+                repository: r.resolve(ActivitySubscriptionRepository.self)!
+            )
+        }
+        
+        container.register(SubscribeToCategoryUseCase.self) { r in
+            SubscribeToCategoryUseCase(
+                repository: r.resolve(ActivitySubscriptionRepository.self)!
+            )
+        }
+        
+        container.register(UnsubscribeFromCategoryUseCase.self) { r in
+            UnsubscribeFromCategoryUseCase(
+                repository: r.resolve(ActivitySubscriptionRepository.self)!
+            )
+        }
+        
+        container.register(GetTodayAssignedExercisesUseCase.self) { r in
+            GetTodayAssignedExercisesUseCase(
+                repository: r.resolve(ActivitySubscriptionRepository.self)!
+            )
+        }
+        
+        container.register(AssignDailyExercisesUseCase.self) { r in
+            AssignDailyExercisesUseCase(
+                repository: r.resolve(ActivitySubscriptionRepository.self)!
+            )
+        }
+        
+        container.register(CompleteExerciseUseCase.self) { r in
+            CompleteExerciseUseCase(
+                repository: r.resolve(ActivitySubscriptionRepository.self)!
+            )
+        }
+        
+        // Reaction Use Cases
+        container.register(ToggleLikeUseCase.self) { r in
+            ToggleLikeUseCase(
+                repository: r.resolve(ReactionRepository.self)!
+            )
+        }
+        
+        container.register(HasUserLikedBlogUseCase.self) { r in
+            HasUserLikedBlogUseCase(
+                repository: r.resolve(ReactionRepository.self)!
+            )
+        }
+        
+        container.register(GetLikesForBlogUseCase.self) { r in
+            GetLikesForBlogUseCase(
+                repository: r.resolve(ReactionRepository.self)!
+            )
+        }
+        
+        // Comment Use Cases
+        container.register(GetCommentsForBlogUseCase.self) { r in
+            GetCommentsForBlogUseCase(
+                repository: r.resolve(CommentRepository.self)!
+            )
+        }
+        
+        container.register(AddCommentUseCase.self) { r in
+            AddCommentUseCase(
+                repository: r.resolve(CommentRepository.self)!
+            )
+        }
+        
+        container.register(UpdateCommentUseCase.self) { r in
+            UpdateCommentUseCase(
+                repository: r.resolve(CommentRepository.self)!
+            )
+        }
+        
+        container.register(DeleteCommentUseCase.self) { r in
+            DeleteCommentUseCase(
+                repository: r.resolve(CommentRepository.self)!
+            )
+        }
+        
+        container.register(AddReplyUseCase.self) { r in
+            AddReplyUseCase(
+                repository: r.resolve(CommentRepository.self)!
             )
         }
     }
